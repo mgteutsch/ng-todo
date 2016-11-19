19020b0ac1f69efaeba9c647281201ed74a33f1a
Mike Teutsch <mgt0117@gmail.com>
2016-11-19 10:42:16 -0600

    Initial commit for routing

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -4,3 +4,18 @@
 app.run(function(FIREBASE_CONFIG){
 	firebase.initializeApp(FIREBASE_CONFIG);
 });
+
+//config runs first, and run (above) runs second
+//you cannot use $scope in .config: the DOM hasn't loaded yet
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/items/list', {	//we are creating routes here... for the to-do list, the create new items
+			templateUrl: 'partials/item-list.html', //DO NOT CAPITALIZE ALL OF Url!!!!!!!!!
+			controller: 'ItemListCtrl'
+		}) 
+		.when('/items/new',{ //another route for the URL
+			templateUrl: 'partials/item-new.html',
+			controller: 'ItemNewCtrl'
+		})
+		.otherwise('/items/list') // if the user types in random stuff at the end of the url, it will redirect to whatever.com/items/list
+});
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,92 +1,9 @@
 "use strict";
 
-var app = angular.module("TodoApp", []);
+var app = angular.module("TodoApp", ["ngRoute"]);
 
-<!--// These are some methods, not using them though //-->
-//******************************************************//
-
-//app.config(function(){ ...NOT USING THIS...
-//
-//})
-
-//app.value("FB", 123456789) //just some number ...NOT USING THIS...
-
-//$http.get() ...NOT USING THIS...
-//******************************************************//
-<!--// Ok, here is what we are using below.. //-->
-
-
-//Firebase ----------------------------------------------------------
-
-// app.constant("FIREBASE_CONFIG", { //the following comes from the red button in firebase ("Add Firebase to your Web App")
-// 	apiKey: "",
-//     authDomain: "",
-//     databaseURL: "",
-//     storageBucket: "",
-//     messagingSenderId: ""
-// }); //don't forget to put in the <script> tag in html
-// ----------------------> took this out, put it in AppConstants.js
-
-// app.run(function(FIREBASE_CONFIG){
-// 	firebase.initializeApp(FIREBASE_CONFIG);
-// }); -----------> took this out, put it in AppConfig.js
-
-// -------------------------------------------------------------------
-
-
-//Controllers and such: (now moved to individual .js files)
-
-// app.controller("NavCtrl", function($scope){
-// 	$scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
-//});
-// ----> moved to NavCtrl.js
-
-
-// ----> moved this next stuff to TodoCtrl.js
-// app.controller("TodoCtrl", function($scope){
-// 	$scope.welcome = "hello";
-// 	$scope.showListView = true;
-// 	$scope.newTask = {}; // empty object
-// 	$scope.items = [
-// 		{
-// 			id: 0,
-// 			task: "mow the lawn",
-// 			isCompleted: true,
-// 			assignedTo: "Zoe"
-// 		},
-// 		{
-// 			id: 1,
-// 			task: "grade quizzes",
-// 			isCompleted: false,
-// 			assignedTo: "William"
-// 		},
-// 		{
-// 			id: 2,
-// 			task: "take nap",
-// 			isCompleted: false,
-// 			assignedTo: "Zoe"
-// 		}
-// 	]
-
-// 	$scope.allItems = function(){
-// 		console.log("You clicked 'All Items'");
-// 		$scope.showListView = true;
-// 	}
-
-// 	$scope.newItem = function(){
-// 		console.log("You clicked 'New Item'");
-// 		$scope.showListView = false;
-// 	}
-
-// 	$scope.addNewItem = function() {
-// 		$scope.newTask.isCompleted = false;
-// 		$scope.newTask.id = $scope.items.length; //this will start with 3 and keep going from there
-// 		console.log("newTask in function:", $scope.newTask);
-// 		$scope.items.push($scope.newTask);
-// 		$scope.newTask = {}; //clears the values after you click the button to submit the new to-do
-// 		$scope.showListView = true; //switches the view to see the to-do list
-// 	}
-
-// });
+//in order to use angular-route package, we must type in "ngRoute".... 
+//go to angular-route.js to find the correct terminology
+//if you use a different installer (like npm), the "ngRoute" will be different. You need to look it up   
 
 
