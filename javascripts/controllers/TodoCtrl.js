a0b56478f65d564bb639bec94b9ceee4f52bc5f7
Mike Teutsch <mgt0117@gmail.com>
2016-11-19 09:52:26 -0600

    finished delete

--- javascripts/controllers/TodoCtrl.js
+++ javascripts/controllers/TodoCtrl.js
@@ -26,12 +26,20 @@ app.controller("TodoCtrl", function($scope, ItemFactory){ //needed to inject Ite
 	// 	// } 
 	// ]
 	//************************************** 
-	$scope.items = []; //-----> all that commented out stuff went to the Seed.json file
+	$scope.items = []; //-----> all that commented out stuff went to the Seed.json file, so now we just have an empty array
 
-	ItemFactory.getItemList().then(function(fbItems){
-		//console.log("Items from controller:", fbItems);
-		$scope.items = fbItems; //This returns all the database items!
-	})
+	
+
+	let getItems = function(){
+		ItemFactory.getItemList().then(function(fbItems){
+			//console.log("Items from controller:", fbItems);
+			$scope.items = fbItems; //This returns all the database items!
+		})
+	}
+	
+	getItems();
+
+
 
 	$scope.allItems = function(){
 		console.log("You clicked 'All Items'");
@@ -45,11 +53,21 @@ app.controller("TodoCtrl", function($scope, ItemFactory){ //needed to inject Ite
 
 	$scope.addNewItem = function() {
 		$scope.newTask.isCompleted = false;
-		$scope.newTask.id = $scope.items.length; //this will start with 3 and keep going from there
-		console.log("newTask in function:", $scope.newTask);
-		$scope.items.push($scope.newTask);
-		$scope.newTask = {}; //clears the values after you click the button to submit the new to-do
-		$scope.showListView = true; //switches the view to see the to-do list
+		//$scope.newTask.id = $scope.items.length; //this will start with 3 and keep going from there
+		//console.log("newTask in function:", $scope.newTask);
+		//$scope.items.push($scope.newTask);
+		ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+			getItems();
+			$scope.newTask = {}; //clears the values after you click the button to submit the new to-do
+			$scope.showListView = true; //switches the view to see the to-do list
+		})	
+	}
+
+	$scope.deleteItem = function(itemId) { //need to pass in the id in the html... item.id
+		console.log("You deleted:", itemId);
+		ItemFactory.deleteItem(itemId).then(function(response){
+			getItems(); //this refreshes the DOM after you've deleted
+		})
 	}
 
 });
